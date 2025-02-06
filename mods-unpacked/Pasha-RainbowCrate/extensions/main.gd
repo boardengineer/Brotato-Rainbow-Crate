8bf6d9032bf70f8ce29eebdcf74a1724c2b60224
boardengineer <77598527+boardengineer@users.noreply.github.com>
2025-02-06 15:22:10 -0800

    Use Consumable pool for version 1.1.9

--- mods-unpacked/Pasha-RainbowCrate/extensions/main.gd
+++ mods-unpacked/Pasha-RainbowCrate/extensions/main.gd
@@ -12,7 +12,11 @@ func maybe_spawn_rainbow_crate(unit:Unit) -> bool:
 	if not rainbow_crate_options.rainbow_crates_enabled:
 		return false
 	
-	var luck = (Utils.get_stat("stat_luck", 0) / 100.0)
+	var luck: = 0.0
+
+	for player_index in RunData.get_player_count():
+		luck += Utils.get_stat("stat_luck", player_index) / 100.0
+	
 	var drop_chance = min(1.0, unit.stats.base_drop_chance * (1 + luck))
 	
 	if RunData.current_wave > RunData.nb_of_waves:
@@ -27,19 +31,24 @@ func maybe_spawn_rainbow_crate(unit:Unit) -> bool:
 		if unit.stats.always_drop_consumables and unit.stats.item_drop_chance >= 1.0 and RunData.current_wave <= RunData.nb_of_waves:
 			item_chance = (rainbow_crate_options.rainbow_crates_drop_chance / (rainbow_crate_options.rainbow_crates_drop_chance + 1.0))
 		
+			
 		if randf() <= item_chance:
 			consumable_to_spawn = rainbow_crate
 		
 			var dist = rand_range(50, 100 + unit.stats.gold_spread)
-			var consumable = consumable_scene.instance()
+			var consumable:Consumable = _consumable_pool.pop_back()
+			if consumable == null:
+				consumable = consumable_scene.instance()
+				_consumables_container.call_deferred("add_child", consumable)
+				var _error = consumable.connect("picked_up", self, "on_consumable_picked_up")
+				yield (consumable, "ready")
+			
 			consumable.consumable_data = consumable_to_spawn
 			consumable.global_position = pos
-			_consumables_container.call_deferred("add_child", consumable)
-			consumable.call_deferred("set_texture", consumable_to_spawn.icon)
+			consumable.set_texture(consumable_to_spawn.icon)
 			var _error = consumable.connect("picked_up", self, "on_consumable_picked_up")
 			consumable.push_back_destination = Vector2(rand_range(pos.x - dist, pos.x + dist), rand_range(pos.y - dist, pos.y + dist))
+			consumable.drop(pos, 0, consumable.push_back_destination)
 			_consumables.push_back(consumable)
-		
 			return true
-	
 	return false
