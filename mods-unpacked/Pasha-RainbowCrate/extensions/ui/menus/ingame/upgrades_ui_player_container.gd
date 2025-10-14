d16c32bfeb301f471c3478de0613ab2a77968d29
boardengineer <77598527+boardengineer@users.noreply.github.com>
2025-10-14 11:48:09 -0700

    Add cursed items (1/N)
    Just add all cursed items, untested dlc check

--- mods-unpacked/Pasha-RainbowCrate/extensions/ui/menus/ingame/upgrades_ui_player_container.gd
+++ mods-unpacked/Pasha-RainbowCrate/extensions/ui/menus/ingame/upgrades_ui_player_container.gd
@@ -19,21 +19,22 @@ func prep_rainbow_crate_options() -> void:
 		if rainbow_crate_options.unlocked_items_only and not ProgressData.items_unlocked.has(item_data.my_id):
 			continue
 		
-		var item_ui = rainbow_crate_item_ui.instance()
-		rainbow_crate_ui.call_deferred("add_item_ui", item_ui)
-		item_ui.parent_box_ui = self
-		
-		item_ui.call_deferred("set_item_data", item_data)
+		_add_item_to_container(item_data)
+		var dlc = ProgressData.get_dlc_data("abyssal_terrors")
+		if dlc:
+			var cursed_item_data = item_data.duplicate()
+			cursed_item_data = dlc.curse_item(cursed_item_data, player_index, true)
+			_add_item_to_container(cursed_item_data)
 		
 	rainbow_crate_ui.call_deferred("hide")
 
 
-func show_upgrades_for_level(level:int)->void :
+func show_upgrades_for_level(level:int) -> void:
 	rainbow_crate_ui.hide()
 	.show_upgrades_for_level(level)
 
 
-func show_item(item_data:ItemParentData)->void :
+func show_item(item_data:ItemParentData) -> void:
 	if item_data.my_id != "consumable_pasha_rainbow_crate":
 		rainbow_crate_ui.hide()
 		.show_item(item_data)
@@ -43,3 +44,10 @@ func show_item(item_data:ItemParentData)->void :
 	_items_container.hide()
 	_upgrades_container.hide()
 	rainbow_crate_ui.show()
+
+
+func _add_item_to_container(item_data) -> void:
+	var item_ui = rainbow_crate_item_ui.instance()
+	rainbow_crate_ui.call_deferred("add_item_ui", item_ui)
+	item_ui.parent_box_ui = self
+	item_ui.call_deferred("set_item_data", item_data)
